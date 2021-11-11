56487789ad92121a48292505f3cf5f3c109d93bb
shadialahmd <shadi.alahmd@gmail.com>
2021-11-11 11:28:31 +0400

    create files

--- data.php
+++ data.php
@@ -5,8 +5,8 @@ header('Content-Type: application/json');
 //database
 define('DB_HOST', '127.0.0.1');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root123');
-define('DB_NAME', 'mydb');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'chartjs');
 
 //get connection
 $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
